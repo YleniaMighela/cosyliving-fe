7c3febb792b83fb137d65d54b0576103ea0f2194
Alessandro Bianchi <ale.bianchi22@hotmail.it>
2025-03-25 16:27:06 +0100

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,33 @@
+// import { Link } from "react-router-dom"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagram, faFacebook, faWhatsapp } from "@fortawesome/free-brands-svg-icons"
+
+
 export default function Footer() {
 
     return (
         <footer>
-            <h1>sono il footer</h1>
+
+            <div className="footer-left">
+                <span>Termini e condizioni</span>
+                <span>Contatti  +39 3324565784</span>
+                <span>email@outlook.it</span>
+                <span>P.IVA 56546841354864</span>
+            </div>
+            <div className="footer-right">
+                <span>Seguici anche su</span>
+                <span><FontAwesomeIcon icon={faInstagram} size="2x" color="#E1306C" /></span>
+                <span><FontAwesomeIcon icon={faFacebook} size="2x" color="#1877F2" /></span>
+                <span><FontAwesomeIcon icon={faWhatsapp} size="2x" color="#25D366" /></span>
+            </div>
+
+
+
 
         </footer>
 
     )
 }
+
+
 
