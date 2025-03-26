48dfd7fe7c8ca7656fa92a6838577e55262bc497
Alessandro Bianchi <ale.bianchi22@hotmail.it>
2025-03-26 11:18:03 +0100

    fix cardtips

--- src/components/CardTips.jsx
+++ src/components/CardTips.jsx
@@ -6,7 +6,7 @@ export default function CardTips() {
         <section className="bg-container">
             <div className="container-tips">
                 <div className="tips-left">
-                    <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat dolorem libero maxime architecto vel harum debitis, soluta laboriosam sed iste corporis doloremque cumque laudantium expedita eveniet saepe commodi consequuntur sapiente.</div>
+                    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, dicta quod rerum, architecto perspiciatis corporis inventore vitae nulla nihil, alias tempore iure obcaecati sint deserunt iste minima similique non mollitia! Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit aspernatur, esse minima, obcaecati beatae accusantium animi cum dolores ipsam nemo accusamus voluptate molestiae dignissimos tempora praesentium maiores corrupti. Eaque, non? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat dolorem libero maxime architecto vel harum debitis, soluta laboriosam sed iste corporis doloremque cumque laudantium expedita eveniet saepe commodi consequuntur sapiente.</div>
                 </div>
 
                 <div className="tips-right">
