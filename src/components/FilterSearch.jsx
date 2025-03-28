281afd6e9525368f22189232a397b7b37a3233c4
marco-gulino <marcogulino98@gmail.com>
2025-03-28 09:27:40 +0100

    [feat] import axios in FilterSearch

--- src/components/FilterSearch.jsx
+++ src/components/FilterSearch.jsx
@@ -1,28 +1,28 @@
 import { useLocation } from "react-router-dom";
-
+import axios from "axios";
 // searchproducts
 
-
 export default function FilterSearch() {
-    const location = useLocation(); // Recupera l'URL attuale
+  const location = useLocation(); // Recupera l'URL attuale
 
-    const isHiddenPage = location.pathname.startsWith("/404")
-    // location.pathname.startsWith("/trip") ||
-    // location.pathname.startsWith("/addPassengers");
+  const isHiddenPage = location.pathname.startsWith("/404");
+  // location.pathname.startsWith("/trip") ||
+  // location.pathname.startsWith("/addPassengers");
 
-
-    return (
-
-        <div className="form_search">
-            {!isHiddenPage && (
-                <>
-                    <input className="input_search" type="search" placeholder="Cerca il tuo prodotto..." />
-                    <button className="button_search" type="submit">Cerca</button>
-                </>
-            )}
-
-        </div>
-    )
-
+  return (
+    <div className="form_search">
+      {!isHiddenPage && (
+        <>
+          <input
+            className="input_search"
+            type="search"
+            placeholder="Cerca il tuo prodotto..."
+          />
+          <button className="button_search" type="submit">
+            Cerca
+          </button>
+        </>
+      )}
+    </div>
+  );
 }
-
