8aefd64fe2ad025b157b3d9bc6070298c12bf34b
Andrea Aceto <andrea.aceto20@gmail.com>
2025-03-31 09:55:55 +0200

    [bug]Price is not displayed correctly

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,44 +1,47 @@
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import CartCard from "../components/CartCard";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 
 export default function Cart() {
 
-    const [cart, setCart] = useState(JSON.parse(localStorage.getItem("Cart")) || [])
-    console.log(cart);
-    // product = cart[0]
-    console.log(cart);
+    const [cart, setCart] = useState(JSON.parse(localStorage.getItem("Cart")) || []);
+    const [prezzo, setPrezzo] = useState(0);
 
+    // Effettua il calcolo del prezzo ogni volta che il carrello cambia
     useEffect(() => {
+        // Salva il carrello in localStorage
+
         localStorage.setItem("Cart", JSON.stringify(cart));
+        // Calcola il totale dei prezzi
+        const total = cart.reduce((sum, item) => sum + item.price * item.quantity, 0);
+
+        // Aggiorna il prezzo
+        setPrezzo(total);
     }, [cart]);
+
     return (
         <>
-            {cart.length === 0 &&
+            {cart.length === 0 ? (
                 <div className="container">
                     <FontAwesomeIcon icon={faCartShopping} className="empty_cart" />
                     <h2>Il tuo carrello è vuoto</h2>
                     <Link to="/"><button>Continua i tuoi acquisti</button></Link>
                 </div>
-
-            }
-            {cart.length != 0 &&
+            ) : (
                 <section>
                     <div className="flex_cart">
                         <CartCard dati={cart} setCart={setCart} />
                     </div>
-                    <Link to="/form"> <button className="button_ordina">Ordina</button></Link>
+                    <Link to="/form">
+                        <button className="button_ordina">Ordina</button>
+                    </Link>
+                    <div>
+                        <h2>Totale : €{prezzo.toFixed(2)}</h2>
+                    </div>
                 </section>
-
-            }
+            )}
         </>
-
-    )
-
-
+    );
 }
-
-
