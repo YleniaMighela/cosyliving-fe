ca44b5608ab2dc6095a94c569f4790cd55a5cb88
Ylenia <yleniamighela@gmail.com>
2025-03-31 14:43:29 +0200

    aggiunto vai al dettaglio nel search

--- src/pages/SearchProducts.jsx
+++ src/pages/SearchProducts.jsx
@@ -142,10 +142,12 @@ export default function SearchProduct() {
         <div className="products-grid " >
           {searchRes.length > 0 ? (
             searchRes.map((res) => (
-              <Link to={`/products/${res.slug}`} key={res.id}>
+              <div key={res.id} className="product-item">
                 <CardProduct prodInfos={res} />
-              </Link>
-
+                <Link to={`/products/${res.slug}`} className="not_link product-link">
+                  <button className="bottone_dettaglio">Vai al dettaglio</button>
+                </Link>
+              </div>
             ))
           ) : (
             <span>Nessun Risultato Trovato</span>
