fbbe7b3f4a4eb46f1211d23fcbf2841f6c7e8bfc
Ylenia <yleniamighela@gmail.com>
2025-04-01 09:38:18 +0200

    modificato css

--- src/components/CardNewArrivals.jsx
+++ src/components/CardNewArrivals.jsx
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
+import NewArrivalPage from "../pages/NewArrivalPage";
 
 export default function NewArrivalsCard() {
     return (
-        <div className="container_imgprice">
-            <h3>New Arrivals</h3>
-            <Link to="/new-arrivals">
-                <img id="special" src="../img/divano.jpg" alt="Nuovi Arrivi" />
-            </Link>
-        </div>
+        <>
+            <div className="container_imgprice">
+                <Link to="/new-arrivals"><h3>New Arrivals</h3>    </Link>
+
+                <img id="special" src="../img/poltrona.jpg" alt="Nuovi Arrivi" />
+                <NewArrivalPage />
+            </div>
+
+        </>
     );
 }
