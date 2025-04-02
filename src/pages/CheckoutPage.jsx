02d61d7d10736bfaa52f503250837b4c3353c91f
Andrea Aceto <andrea.aceto20@gmail.com>
2025-04-02 13:08:51 +0200

    [bug]

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -65,8 +65,6 @@ export default function FormCliente() {
       })
       .then((response) => {
         setOrderProducts(response.data);
-
-
       })
       .catch((err) => {
         console.log(err);
@@ -90,9 +88,6 @@ export default function FormCliente() {
       })
       .then((response) => {
         setTot_price(response.data.tot_price)
-
-
-
       })
       .catch((err) => {
         console.log(err);
@@ -202,27 +197,30 @@ export default function FormCliente() {
       totalPrice: prezzo_totale.toFixed(2),
     };
 
-    axios
-      .post(`http://localhost:3000/order/`, totalInfo, {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-      .then((response) => {
+    axios.post("http://localhost:3000/order/", totalInfo, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then(() => {
         localStorage.clear();
         setCart([]);
-
+        console.log("LocalStorage after clearing:", localStorage.getItem("Cart"));
+        window.location.href = "/order-summary";
       })
       .catch((err) => {
         console.error(err);
-        setErrorMessage("Abbiamo riscontrato un errore");
+        setErrorMessage("Abbiamo riscontrato un errore, riprova.");
       });
+
 
 
     handlePersonalSubmit(e);
 
     //Reindirizza alla pagina degli ordini
-    window.location.href = "/order-summary";
+    setInterval(() => {
+      window.location.href = "/order-summary";
+    }, 3000);
   }
 
   useEffect(() => {
@@ -290,165 +288,164 @@ export default function FormCliente() {
               </>
             )}
           </div>
-        </section>
+          {/* Form Dati Personali */}
+          <section>
+            <form
+              className="form_personali"
+              onSubmit={(e) => {
+                handleSubmit(e);
+              }}
+            >
+              <h2>Inserisci Dati Personali</h2>
+              <div>
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Nome...*"
+                  value={personalData.name}
+                  onChange={handlePersonalData}
+                  required
+                />
+              </div>
+              <div>
+                <input
+                  type="text"
+                  name="surname"
+                  placeholder="Cognome...*"
+                  value={personalData.surname}
+                  onChange={handlePersonalData}
+                  required
+                />
+              </div>
+              <div>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Email...*"
+                  value={personalData.email}
+                  onChange={handlePersonalData}
+                  required
+                />
+              </div>
+              <div>
+                <input
+                  type="tel"
+                  name="phone_num"
+                  placeholder="Numero telefonico...*"
+                  value={personalData.phone_num}
+                  onChange={handlePersonalData}
+                  required
+                />
+              </div>
+              <div>
+                <input
+                  type="text"
+                  name="cf"
+                  placeholder="Codice Fiscale...*"
+                  value={personalData.cf}
+                  onChange={handlePersonalData}
+                  required
+                />
+              </div>
+              <div>
+                <input
+                  type="text"
+                  name="shipment_address"
+                  value={personalData.shipment_address}
+                  placeholder="Via...*"
+                  onChange={handlePersonalData}
+                  required
+                />
+              </div>
+              <div>
+                <input
+                  type="text"
+                  name="cap"
+                  value={personalData.cap}
+                  placeholder="CAP...*"
+                  onChange={handlePersonalData}
+                  required
+                />
+              </div>
+              <div>
+                <input
+                  type="text"
+                  name="city"
+                  value={personalData.city}
+                  placeholder="Città...*"
+                  onChange={handlePersonalData}
+                  required
+                />
+              </div>
+              {errorMessage}
+              <div>
+                <button type="submit">Invia dati</button>
+              </div>
+            </form>
+          </section >
 
+          {/* sezione dati fatturazione*/}
+          < div >
+            {/* Form Dati di Fatturazione */}
+            <section>
+              < form className="form_personali" onSubmit={handleBillingSubmit} >
+                <h2>Inserisci Dati di Fatturazione</h2>
+                <div>
+                  <input
+                    type="text"
+                    name="name_billing"
+                    placeholder="Nome..."
+                    value={billingData.name_billing}
+                    onChange={handleBillingData}
+                    required
+                  />
+                </div>
+                <div>
+                  <input
+                    type="text"
+                    name="surname_billing"
+                    placeholder="Cognome..."
+                    value={billingData.surname_billing}
+                    onChange={handleBillingData}
+                    required
+                  />
+                </div>
+                <div>
+                  <input
+                    type="text"
+                    name="billing_address"
+                    placeholder="Indirizzo di Fatturazione..."
+                    value={billingData.billing_address}
+                    onChange={handleBillingData}
+                    required
+                  />
+                </div>
+                <div>
+                  <input
+                    type="text"
+                    name="city_billing"
+                    value={billingData.city_billing}
+                    placeholder="Città..."
+                    onChange={handleBillingData}
+                    required
+                  />
+                </div>
+                <div>
+                  <input
+                    type="text"
+                    name="cap_billing"
+                    value={billingData.cap_billing}
+                    placeholder="CAP..."
+                    onChange={handleBillingData}
+                    required
+                  />
+
+                </div>
+              </form >
+            </section>
+          </div>
       </div>
-      {/* Form Dati Personali */}
-      <section>
-        <form
-          className="form_personali"
-          onSubmit={(e) => {
-            handleSubmit(e);
-          }}
-        >
-          <h2>Inserisci Dati Personali</h2>
-          <div>
-            <input
-              type="text"
-              name="name"
-              placeholder="Nome...*"
-              value={personalData.name}
-              onChange={handlePersonalData}
-              required
-            />
-          </div>
-          <div>
-            <input
-              type="text"
-              name="surname"
-              placeholder="Cognome...*"
-              value={personalData.surname}
-              onChange={handlePersonalData}
-              required
-            />
-          </div>
-          <div>
-            <input
-              type="email"
-              name="email"
-              placeholder="Email...*"
-              value={personalData.email}
-              onChange={handlePersonalData}
-              required
-            />
-          </div>
-          <div>
-            <input
-              type="tel"
-              name="phone_num"
-              placeholder="Numero telefonico...*"
-              value={personalData.phone_num}
-              onChange={handlePersonalData}
-              required
-            />
-          </div>
-          <div>
-            <input
-              type="text"
-              name="cf"
-              placeholder="Codice Fiscale...*"
-              value={personalData.cf}
-              onChange={handlePersonalData}
-              required
-            />
-          </div>
-          <div>
-            <input
-              type="text"
-              name="shipment_address"
-              value={personalData.shipment_address}
-              placeholder="Via...*"
-              onChange={handlePersonalData}
-              required
-            />
-          </div>
-          <div>
-            <input
-              type="text"
-              name="cap"
-              value={personalData.cap}
-              placeholder="CAP...*"
-              onChange={handlePersonalData}
-              required
-            />
-          </div>
-          <div>
-            <input
-              type="text"
-              name="city"
-              value={personalData.city}
-              placeholder="Città...*"
-              onChange={handlePersonalData}
-              required
-            />
-          </div>
-          {errorMessage}
-          <div>
-            <button type="submit">Invia dati</button>
-          </div>
-        </form>
-      </section >
 
-      {/* sezione dati fatturazione*/}
-      < div >
-        {/* Form Dati di Fatturazione */}
-        <section>
-          < form className="form_personali" onSubmit={handleBillingSubmit} >
-            <h2>Inserisci Dati di Fatturazione</h2>
-            <div>
-              <input
-                type="text"
-                name="name_billing"
-                placeholder="Nome..."
-                value={billingData.name_billing}
-                onChange={handleBillingData}
-                required
-              />
-            </div>
-            <div>
-              <input
-                type="text"
-                name="surname_billing"
-                placeholder="Cognome..."
-                value={billingData.surname_billing}
-                onChange={handleBillingData}
-                required
-              />
-            </div>
-            <div>
-              <input
-                type="text"
-                name="billing_address"
-                placeholder="Indirizzo di Fatturazione..."
-                value={billingData.billing_address}
-                onChange={handleBillingData}
-                required
-              />
-            </div>
-            <div>
-              <input
-                type="text"
-                name="city_billing"
-                value={billingData.city_billing}
-                placeholder="Città..."
-                onChange={handleBillingData}
-                required
-              />
-            </div>
-            <div>
-              <input
-                type="text"
-                name="cap_billing"
-                value={billingData.cap_billing}
-                placeholder="CAP..."
-                onChange={handleBillingData}
-                required
-              />
-
-            </div>
-          </form >
-        </section>
-      </div >
     </>
   );
 }
