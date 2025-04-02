1606e1fd3c96f598f0495c30fea90bab318037f8
marco-gulino <marcogulino98@gmail.com>
2025-04-02 17:36:34 +0200

    [feat] fix axio call endpoint in ResultCategoryHome

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCartShopping, faHeart, faHouse } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCartShopping,
+  faHeart,
+  faHouse,
+} from "@fortawesome/free-solid-svg-icons";
 import FilterSearch from "./FilterSearch";
-
 
 export default function Header() {
   const categories = [
@@ -24,14 +27,17 @@ export default function Header() {
           </h1>
           <div className="font-block">
             <FontAwesomeIcon icon={faHouse} size="2x" />
-
           </div>
         </Link>
 
         {/* Navbar Desktop */}
         <nav className="categories-desktop">
           {categories.map((category) => (
-            <Link key={category} to={`/category/${category}`} className="category-link">
+            <Link
+              key={category}
+              to={`/category/${category}`}
+              className="category-link"
+            >
               {category}
             </Link>
           ))}
@@ -56,7 +62,11 @@ export default function Header() {
       {/* Sezione Icone */}
       <div className="icons_header">
         <Link to="/cart">
-          <FontAwesomeIcon icon={faCartShopping} size="2x" className="cart-icon" />
+          <FontAwesomeIcon
+            icon={faCartShopping}
+            size="2x"
+            className="cart-icon"
+          />
         </Link>
         <Link to="/wishlist">
           <FontAwesomeIcon icon={faHeart} size="2x" className="heart-icon" />
