8b6b6a884022a9aaf8147732cbdd53b92e6245e7
marco-gulino <marcogulino98@gmail.com>
2025-04-03 19:33:46 +0200

    [fix] adjust prop slug value in CartCard

--- src/components/CartCard.jsx
+++ src/components/CartCard.jsx
@@ -1,84 +1,87 @@
-import { useState, useEffect } from "react"
-import { Link } from "react-router-dom"
-import Cart from "../pages/CartPage"
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Cart from "../pages/CartPage";
 
 export default function CartCard({ dati, setCart }) {
-    // console.log(dati);
+  // console.log(dati);
 
+  const [data, setData] = useState(dati.props || []);
+  // console.log(localStorage.getItem("Cart"));
 
-    const [data, setData] = useState(dati.props || [])
-    // console.log(localStorage.getItem("Cart"));
+  function EliminateArticle(id) {
+    var Cart = JSON.parse(localStorage.getItem("Cart")) || [];
 
-    function EliminateArticle(id) {
-        var Cart = JSON.parse(localStorage.getItem("Cart")) || []
+    Cart = Cart.filter((product) => product.id != id);
+    setCart(Cart);
+    localStorage.setItem("Cart", JSON.stringify(Cart));
 
-        Cart = Cart.filter(product => product.id != id)
-        setCart(Cart)
-        localStorage.setItem("Cart", JSON.stringify(Cart))
+    setData(Cart);
+    window.dispatchEvent(new Event("storage"));
+  }
+  console.log(data);
 
-        setData(Cart)
-        window.dispatchEvent(new Event("storage"));
-    }
-    console.log(data);
+  function updateQuantity(id, newQuantity) {
+    var newCart = data.map((product) =>
+      product.id === id ? { ...product, quantity: newQuantity } : product
+    );
+    setData(newCart);
+    setCart(newCart);
 
-    function updateQuantity(id, newQuantity) {
-        var newCart = data.map((product) => product.id === id ? { ...product, quantity: newQuantity } : product)
-        setData(newCart);
-        setCart(newCart)
+    localStorage.setItem("Cart", JSON.stringify(newCart));
+    console.log(localStorage.getItem("Cart"));
+  }
 
-        localStorage.setItem("Cart", JSON.stringify(newCart))
-        console.log(localStorage.getItem("Cart"));
+  useEffect(() => {
+    const storedCart = JSON.parse(localStorage.getItem("Cart")) || [];
+    setData(storedCart);
+  }, []);
 
-    }
-
-    useEffect(() => {
-        const storedCart = JSON.parse(localStorage.getItem("Cart")) || [];
-        setData(storedCart);
-    }, []);
-
-
-    return (
-        data.map((prop) => (
-
-            <div className="container_cart" key={prop.id}>
-                <Link to={`/products/${prop.slug}`}> <img src={prop.img} alt={prop.name} /></Link>
-                <div>
-                    <h2>{prop.name}</h2>
-                    <p>€{prop.discount_price || prop.price}</p>
-                    <div className="flex_cart">
-                        <button className="button_cart"
-                            onClick={() =>
-                                prop.quantity === 1
-                                    ? EliminateArticle(prop.id)
-                                    : updateQuantity(prop.id, prop.quantity - 1)
-                            }
-                        >
-                            -
-                        </button>
-                        <p>{prop.quantity}</p>
-                        {prop.quantity + 1 > prop.q_max ? (
-
-                            <>
-                                <button className="button_cart" >
-                                    +
-                                </button>
-                                <br />
-                                <p className="max_quantity_message" > Quantità massima raggiunta</p>
-                            </>
-
-                        ) : (
-                            <button className="button_cart" onClick={() => updateQuantity(prop.id, prop.quantity + 1)}>
-                                +
-                            </button>
-                        )}
-                    </div>
-                    <button className="button_remove" onClick={() => EliminateArticle(prop.id)}>Rimuovi dal carrello</button>
-                </div>
-
-            </div>
-
-        ))
-
-
-    )
+  return data.map((prop) => (
+    <div className="container_cart" key={prop.id}>
+      <Link to={`/products/${prop.p_slug}`}>
+        {" "}
+        <img src={prop.img} alt={prop.name} />
+      </Link>
+      <div>
+        <h2>{prop.name}</h2>
+        <p>€{prop.discount_price || prop.price}</p>
+        <div className="flex_cart">
+          <button
+            className="button_cart"
+            onClick={() =>
+              prop.quantity === 1
+                ? EliminateArticle(prop.id)
+                : updateQuantity(prop.id, prop.quantity - 1)
+            }
+          >
+            -
+          </button>
+          <p>{prop.quantity}</p>
+          {prop.quantity + 1 > prop.q_max ? (
+            <>
+              <button className="button_cart">+</button>
+              <br />
+              <p className="max_quantity_message">
+                {" "}
+                Quantità massima raggiunta
+              </p>
+            </>
+          ) : (
+            <button
+              className="button_cart"
+              onClick={() => updateQuantity(prop.id, prop.quantity + 1)}
+            >
+              +
+            </button>
+          )}
+        </div>
+        <button
+          className="button_remove"
+          onClick={() => EliminateArticle(prop.id)}
+        >
+          Rimuovi dal carrello
+        </button>
+      </div>
+    </div>
+  ));
 }
