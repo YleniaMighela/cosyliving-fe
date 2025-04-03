4a44c94155593e992df9c00a002b6bd31642c182
Ylenia <yleniamighela@gmail.com>
2025-04-03 14:32:39 +0200

    sistemati bottoni carrello

--- src/components/CartCard.jsx
+++ src/components/CartCard.jsx
@@ -44,7 +44,7 @@ export default function CartCard({ dati, setCart }) {
                     <h2>{prop.name}</h2>
                     <p>€{prop.price}</p>
                     <div className="flex_cart">
-                        <button
+                        <button className="button_cart"
                             onClick={() =>
                                 prop.quantity === 1
                                     ? EliminateArticle(prop.id)
@@ -57,20 +57,20 @@ export default function CartCard({ dati, setCart }) {
                         {prop.quantity + 1 > prop.q_max ? (
 
                             <>
-                                <button >
+                                <button className="button_cart" >
                                     +
                                 </button>
                                 <br />
-                                <p>Quantità massima raggiunta</p>
+                                <p> Quantità massima raggiunta</p>
                             </>
 
                         ) : (
-                            <button onClick={() => updateQuantity(prop.id, prop.quantity + 1)}>
+                            <button className="button_cart" onClick={() => updateQuantity(prop.id, prop.quantity + 1)}>
                                 +
                             </button>
                         )}
                     </div>
-                    <button onClick={() => EliminateArticle(prop.id)}>Rimuovi dal carrello</button>
+                    <button className="button_remove" onClick={() => EliminateArticle(prop.id)}>Rimuovi dal carrello</button>
                 </div>
 
             </div>
