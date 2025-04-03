48101aab0be6925492b59897284124f9cffdd5ed
Alessandro Bianchi <ale.bianchi22@hotmail.it>
2025-04-03 15:01:14 +0200

    fix css and searchbar

--- src/components/CardTips.jsx
+++ src/components/CardTips.jsx
@@ -6,12 +6,12 @@ export default function CardTips() {
         <section className="bg-container">
             <div className="container-tips">
                 <div className="tips-left">
-                    <div> <p>Benvenuto nel tuo spazio di comfort e stile!
+                    <div> <p className="justify">Benvenuto nel tuo spazio di comfort e stile!
                         Scopri il nostro eCommerce dedicato all’arredamento per il salotto, dove design, qualità e comfort si incontrano per trasformare la tua casa in un’oasi di relax. Offriamo una selezione esclusiva di divani, poltrone, pouf, tavolini, sedie, lampade e librerie, pensati per adattarsi a ogni stile e necessità.
 
                         Sia che tu voglia un salotto moderno ed elegante, uno spazio accogliente e classico, o un angolo di design minimalista, troverai materiali pregiati, lavorazioni artigianali e soluzioni innovative per rendere unica la tua zona living.</p>
                         <br></br>
-                        <p>
+                        <p className="justify-centrer">
                             💡 Perché sceglierci?<br></br>
                             ✔ Ampia selezione di prodotti di alta qualità<br></br>
                             ✔ Design esclusivi per ogni esigenza di stile<br></br>
@@ -19,12 +19,12 @@ export default function CardTips() {
                             ✔ Consegna veloce e assistenza clienti dedicata
 
                         </p>
-                        <p>   💭 Immagina il tuo salotto perfetto e lasciati ispirare dalla nostra collezione!<br></br>
+                        <p className="justify-centrer">   💭 Immagina il tuo salotto perfetto e lasciati ispirare dalla nostra collezione!<br></br>
 
                             🛋️ Scopri il catalogo e arreda con gusto!</p>
                     </div>
                 </div>
-
+                <hr className="bar" />
                 <div className="tips-right">
                     <img className="img-tips" src="/tips-img.webp" alt="" />
                 </div>
